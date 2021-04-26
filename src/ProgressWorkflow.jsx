f2ce4f047fb0f4733331a049cb9bac918b9850ce
Alex Medesan <alex.medesan@gmail.com>
2021-04-26 11:09:36 +0300

    fixed show progress in dexterity and other places, removed flicker when going back to home

--- src/ProgressWorkflow.jsx
+++ src/ProgressWorkflow.jsx
@@ -13,7 +13,6 @@ const ProgressWorkflow = (props) => {
   const { content, pathname } = props;
   const currentStateKey = content?.review_state;
   const dispatch = useDispatch();
-
   const [visible, setVisible] = useState(false);
   const [isToolbarOpen, setIsToolbarOpen] = useState(false);
   const [workflowProgressSteps, setWorkflowProgressSteps] = useState([]);
@@ -70,8 +69,10 @@ const ProgressWorkflow = (props) => {
     };
 
     setIsToolbarOpen(!!hasToolbar);
-    // filter out paths that don't have workflow (home, login etc)
+    // filter out paths that don't have workflow (home, login, dexterity even if the content obj stays the same etc)
     if (
+      content['@id'].indexOf(pathname) >= 0 &&
+      pathname !== '/' && // wihout this there will be a flicker for going back to home ('/' in included in all api paths)
       workflowProgress?.result &&
       !workflowProgress.workflow?.error &&
       Array.isArray(workflowProgress?.result?.steps)
@@ -87,10 +88,10 @@ const ProgressWorkflow = (props) => {
       setCurrentState(null); // reset current state only if a path without workflow is
       // chosen to avoid flicker for those that have workflow
     }
-  }, [currentStateKey, workflowProgress]); // eslint-disable-line
+  }, [workflowProgress?.result]); // eslint-disable-line
 
   useEffect(() => {
-    dispatch(getWorkflowProgress(pathname)); // the are paths that don't have workflow (home, login etc)
+    dispatch(getWorkflowProgress(content['@id'])); // the are paths that don't have workflow (home, login etc)
   }, [dispatch, pathname, content]);
 
   // on mount subscribe to mousedown to be able to close on click outside
