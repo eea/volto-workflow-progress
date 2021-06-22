f418ccafb0f2579dab2e9c9434385ac935fa4ac6
David Ichim <ichim.david@gmail.com>
2021-06-22 20:20:48 +0300

    Refs #124587 move state mapping to config settings making them overridable

--- src/customizations/volto/constants/Workflows.js
+++ src/customizations/volto/constants/Workflows.js
@@ -4,29 +4,13 @@
  */
 
 import { last, split } from 'lodash';
+import config from '@plone/volto/registry';
 
 export default function getWorkflowMapping(url, current) {
   const mapping = {
-    publish: { value: 'public', label: 'Public', color: '#007bc1', url },
-    private: { value: 'private', label: 'Private', color: '#ed4033', url },
-    pending: { value: 'pending', label: 'Pending', color: '#f6a808', url },
-    send_back: { value: 'private', label: 'Private', color: '#ed4033', url },
-    retract: { value: 'retract', label: 'Retract', color: '#ed4033', url },
-    submit: { value: 'review', label: 'Review', color: '#f4e037', url },
-    retracted: {
-      value: 'retracted',
-      label: 'Retracted',
-      color: '#ed4033',
-      url,
-    },
-    published: {
-      value: 'published',
-      label: 'Published',
-      color: '#007bc1',
-      url,
-    },
+    ...config?.settings?.workflows?.state_mapping,
   };
-
+  mapping['url'] = url;
   if (url) {
     const key = last(split(url, '/'));
     if (key in mapping) {
--- src/index.js
+++ src/index.js
@@ -7,6 +7,28 @@ const applyConfig = (config) => {
     workflowProgress,
   };
 
+  config.settings.workflows = {
+    ...config.settings.workflows,
+    state_mapping: {
+      publish: { value: 'public', label: 'Public', color: '#007bc1' },
+      private: { value: 'private', label: 'Private', color: '#ed4033' },
+      pending: { value: 'pending', label: 'Pending', color: '#f6a808' },
+      send_back: { value: 'private', label: 'Private', color: '#ed4033' },
+      retract: { value: 'retract', label: 'Retract', color: '#ed4033' },
+      submit: { value: 'review', label: 'Review', color: '#f4e037' },
+      retracted: {
+        value: 'retracted',
+        label: 'Retracted',
+        color: '#ed4033',
+      },
+      published: {
+        value: 'published',
+        label: 'Published',
+        color: '#007bc1',
+      },
+    },
+  };
+
   const appExtras = config.settings.appExtras || [];
 
   config.settings.appExtras = [
