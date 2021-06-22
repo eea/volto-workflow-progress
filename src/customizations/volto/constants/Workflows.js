25862ddf445562a07cb882e3c8e059996178df62
David Ichim <ichim.david@gmail.com>
2021-06-22 19:42:26 +0300

    Refs #124587 updated workflow mapping to reference our naming for published

--- src/customizations/volto/constants/Workflows.js
+++ src/customizations/volto/constants/Workflows.js
@@ -7,13 +7,24 @@ import { last, split } from 'lodash';
 
 export default function getWorkflowMapping(url, current) {
   const mapping = {
-    published: { value: 'public', label: 'Public', color: '#007bc1', url },
     publish: { value: 'public', label: 'Public', color: '#007bc1', url },
     private: { value: 'private', label: 'Private', color: '#ed4033', url },
     pending: { value: 'pending', label: 'Pending', color: '#f6a808', url },
     send_back: { value: 'private', label: 'Private', color: '#ed4033', url },
-    retract: { value: 'private', label: 'Private', color: '#ed4033', url },
+    retract: { value: 'retract', label: 'Retract', color: '#ed4033', url },
     submit: { value: 'review', label: 'Review', color: '#f4e037', url },
+    retracted: {
+      value: 'retracted',
+      label: 'Retracted',
+      color: '#ed4033',
+      url,
+    },
+    published: {
+      value: 'published',
+      label: 'Published',
+      color: '#007bc1',
+      url,
+    },
   };
 
   if (url) {
