663a9d96faf81a98fa70b56320b29f8901ba01d6
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2026-09-04 08:31:09 +0300

    chore: migrate add-on to Volto 19 structure

--- volto.config.js
+++ volto.config.js
@@ -1,10 +1,7 @@
-const addonName = process.env.ADDON_NAME;
-
-if (!addonName) {
-  throw new Error('ADDON_NAME must be set');
-}
+const addons = ['@eeacms/volto-workflow-progress'];
+const theme = '';
 
 module.exports = {
-  addons: [addonName],
-  theme: '',
+  addons,
+  theme,
 };
